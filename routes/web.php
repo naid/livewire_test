3e2b52d25ca7e6cf78c2048a4e6408cc128b0fee
r2 <naid.rm@gmail.com>
2021-10-22 19:05:16 +0800

    Bootstrap UI COmplete
    composer require laravel/ui
    php artisan ui bootstrap
    php artisan ui bootstrap --auth
    npm install && npm run dev
    php artisan migrate

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@
 //Route::livewire('/', 'home');
 Route::get('/' , 'App\Http\Controllers\HomeController@index');
 Route::get('/login' , 'App\Http\Controllers\LoginController@index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
