bcf43a1c243a6ace83bf93dd4c8775bfbd1dc6c3
r2 <naid.rm@gmail.com>
2021-10-22 18:47:53 +0800

    not continuing to turbolinks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <livewire:styles />
 </head>
 <body>
-    @include('layout.navbar')
+    @include('layouts.navbar')
     <livewire:counter />
     <livewire:scripts />
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,6 @@
 //     return view('welcome');
 // });
 
-Route::livewire('/', 'home');
+//Route::livewire('/', 'home');
+Route::get('/' , 'App\Http\Controllers\HomeController@index');
+Route::get('/login' , 'App\Http\Controllers\LoginController@index');
