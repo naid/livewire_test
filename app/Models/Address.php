369f5e3a46c05e2e14f38add0ffb7489cb2114c5
r2 <naid.rm@gmail.com>
2021-10-24 19:56:04 +0800

    ActivityLog complete

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -3,10 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Address extends Model
 {
+    use LogsActivity;
+
     protected $fillable = [
+        'address1',
+        'address2',
+        'suburb',
+        'post_code',
+        'city',
+    ];
+
+    protected static $logAttributes = [
         'address1',
         'address2',
         'suburb',
@@ -17,5 +29,12 @@ class Address extends Model
     public function users()
     {
         return $this->morphedByMany(User::class, 'addressable');
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->logFillable()->logOnlyDirty();
+        // Chain fluent methods for configuration options
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, LogsActivity;
 
     /**
      * The attributes that are mass assignable.
@@ -29,7 +31,18 @@ class User extends Authenticatable
 
     ];
     
+    protected static $logAttributes = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'company_name',
+        'company_email',
 
+    ];
+
+    //LOGGING everything when there's a change in the table
+    //protected static $recordEvents = [ 'updated' ]; //logging only updates to data
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -57,5 +70,12 @@ class User extends Authenticatable
     public function contacts()
     {
         return $this->morphToMany(Contact::class, 'contactable');
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->logFillable()->logOnlyDirty();
+        // Chain fluent methods for configuration options
     }
 }
